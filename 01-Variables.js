e8214db900317e1b2dcd074889e38923787ec0d2
JonY-Vinan <jony.v.c.ecuaor95@gmail.com>
2024-11-27 16:43:38 +0100

    Consepto de variables (var, let, const)

---
+++ 01-Variables.js
@@ -0,0 +1,26 @@
+//Utilidad de las variables
+
+//var
+var helloWorld = "Hola javaScript"
+console.log(helloWorld)
+
+helloWorld = "hola"
+console.log(helloWorld)
+
+
+//let
+let helloWorld2 = "Hola javaScript 2"
+console.log(helloWorld2)
+
+
+
+//const
+
+const helloWorld3 = "hola javaScript 3"
+console.log(helloWorld3)
+
+//Error 
+/*
+helloWorld3 = "hola javaScript 3"
+console.log(helloWorld3)
+*/
