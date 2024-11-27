7e439061d3c4307af13c77be10660a8ea15e23c4
JonY-Vinan <jony.v.c.ecuaor95@gmail.com>
2024-11-27 17:04:11 +0100

    Tipos de datos primitivos

---
+++ 02-datotypes.js
@@ -0,0 +1,47 @@
+// Tipos de datos primitivos
+
+// String ( Cadena de texto )
+const nombre = "Jony"
+const alias = 'ecua'
+const email = `ecua@ecua.com`
+
+// Números ( number )
+let age = 3 // Enteros
+let altura = 1.67 // Decimales
+
+// Booleanos ( Boolean )
+let isStudent = false
+let isTeacher = true
+
+// Undefined
+let undefinedValue
+console.log(undefinedValue)
+
+// null
+let nullVarible = null
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+let entero = BigInt(1234234566432643623462346223452345)
+let entero2 = 12342345664326436146134662346423452345524n
+
+//Mostramos los tipos de datos 
+console.log(typeof nombre)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof altura)
+
+console.log(typeof isTeacher)
+console.log(typeof isStudent)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullVarible)
+
+console.log(typeof entero)
+console.log(typeof entero2)
+
